039b08d384b626639ce6168868460ff6e8332965
Josu-s12 <josuedavidsarmiento@hotmail.com>
2024-04-11 22:22:07 -0500

    Primer Commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const coinRoutes = require('./Routes/coinroutes');
+const usersRoutes = require('./Routes/usersroutes');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Middleware para parsear JSON en las solicitudes
+app.use(express.json());
+
+// Usar las rutas definidas
+app.use('/coin', coinRoutes);
+app.use('/users', usersRoutes);
+
+// Iniciar el servidor
+app.listen(PORT, () => {
+    console.log(`Servidor Express corriendo en el puerto ${PORT}`);
+});
